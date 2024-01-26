2c92cff5739b9f62b5ee7d1be43e6a003f9411a9
Anusprasan <anushprasan20001112@gmail.com>
2024-01-26 13:46:40 +0530

    Added

---
+++ blursmoothening.py
@@ -0,0 +1,9 @@
+import cv2
+
+img =cv2.imread("img7.jpg")
+gaussianImg=cv2.GaussianBlur(img,(41,41),50)
+gaussianImg1=cv2.GaussianBlur(img,(21,21),50)
+cv2.imshow("original ",img)
+cv2.imshow("gaussianImg",gaussianImg)
+cv2.imshow("gaussianImg1",gaussianImg1)
+cv2.waitKey(0)
