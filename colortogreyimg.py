a39297263d955f2062e85c073b5073da287f90f4
Anusprasan <anushprasan20001112@gmail.com>
2024-01-26 12:12:38 +0530

    Added

--- colortogreyimg.py
+++ colortogreyimg.py
@@ -1,3 +1,6 @@
 import cv2
-image=cv2.imread("img7.jpg")
-cv2.imshow("show",image)
+image= cv2.imread("img7.jpg")
+greyImage = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+cv2.imshow('original',image)
+cv2.imshow('Grey',greyImage)
+cv2.waitKey(0)
--- readshowwrite.py
+++ readshowwrite.py
@@ -1,3 +1,4 @@
+import numpy as np
 import cv2
 img = cv2.imread("img5.jpg")
 cv2.imshow('show',img)
