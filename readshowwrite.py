517a481bb2971189525bf53d32714b1a8f497838
Anusprasan <anushprasan20001112@gmail.com>
2024-01-26 11:43:16 +0530

    Added

--- readshowwrite.py
+++ readshowwrite.py
@@ -1,6 +1,6 @@
 import cv2
-img = cv2.imread('img5.jpg')
-cv2.imshow("show",img)
+img = cv2.imread("img5.jpg")
+cv2.imshow('show',img)
 cv2.imwrite('img6.png',img)
 cv2.waitKey(10000)
 cv2.destroyAllWindows()
